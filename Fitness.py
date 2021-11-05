8bf247297862fec5ba28ef4ffd842ff98a269c28
BradYang666 <yangxh0606@gmail.com>
2021-11-05 10:43:59 -0700

    Lesson 2

---
+++ Fitness.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+cardiodata = pd.read_csv(r'C:\Users\yangx\Documents\Python Learning\Kaggle Project\CardioGoodFitness\CardioGoodFitness.csv')
+
+cardiodata.head()
